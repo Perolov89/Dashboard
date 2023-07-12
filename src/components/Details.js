294e068eebb4fcc5ddd93dd106200337a0ea2b11
Olle stålberg <olle@olles-air.home>
2023-07-12 20:13:35 +0200

    fixed return problem in details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -15,22 +15,25 @@ const Details = ({ details }) => {
   const convertMilliontoBillion = (number) => {
     return (number / 1000).toFixed(2);
   };
-  <Card>
-    <ul className="w-full h-full flex flex-col justify-between divide-y-1">
-      {Object.keys(detailsList).map((item) => {
-        return (
-          <li key={item} className="flex-1 flex justify-between items-center">
-            <span>{detailsList[item]}</span>
-            <span>
-              {item === "marketCapitalization"
-                ? `${convertMilliontoBillion(details[item])}B`
-                : details[item]}
-            </span>
-          </li>
-        );
-      })}
-    </ul>
-  </Card>;
+  
+  return(
+    <Card>
+      <ul className="w-full h-full flex flex-col justify-between divide-y-1">
+        {Object.keys(detailsList).map((item) => {
+          return (
+            <li key={item} className="flex-1 flex justify-between items-center">
+              <span>{detailsList[item]}</span>
+              <span>
+                {item === "marketCapitalization"
+                  ? `${convertMilliontoBillion(details[item])}B`
+                  : details[item]}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </Card>
+  );
 };
 
 export default Details;
